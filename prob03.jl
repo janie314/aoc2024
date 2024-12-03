85babe28497078f5d600624c6d22536da596f6c2
12ball <pub@janie.page>
2024-12-03 07:56:43 -0600

    1733234203

--- prob03.jl
+++ prob03.jl
@@ -1,24 +1,20 @@
 
 function a(text)
     matches = eachmatch(r"mul\([0-9]+,[0-9]+\)", text)
-    total_product = 0
-
+    res = 0
     for match in matches
-        numbers = replace(match.match, "mul(" => "", ")" => "") |> v -> split(v, ',') |> x -> parse.(Int, x)
+        numbers = replace(match.match, "mul(" => "", ")" => "") |> v -> split(v, ',') |> n -> parse.(Int, n)
         num1, num2 = numbers
-        total_product += num1 * num2
+        res += num1 * num2
     end
-
-    return total_product
+    return res
 end
 
 
 function b(text)
     while length(findall(r"don't", text)) != 0
-        println("joe")
         text = replace(text, r"don't\(\)[^d]+" => "")
     end
-
     return a(text)
 end
 
