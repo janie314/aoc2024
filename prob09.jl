ea2ba69b5ab3a8bfd82fd10f17af5180c17e67f3
12ball <pub@janie.page>
2024-12-10 21:03:49 -0600

    1733886229

--- prob09.jl
+++ prob09.jl
@@ -40,3 +40,51 @@ end
 
 println("part A")
 println(a(read_input()))
+
+function read_input2()
+    text = strip(read("input", String))
+    disk = Vector{Tuple{Int128,Int128}}()
+    for i in 1:2:length(text)
+        disk = [disk; (Int128((i - 1) / 2), atoi(text[i]))]
+        if i + 1 <= length(text)
+            disk = [disk; (-1, atoi(text[i+1]))]
+        end
+    end
+    disk
+end
+
+function checksum2(v)
+    res = 0
+    k = 0
+    for (m, len) in v
+        for i in 1:len
+            if m != -1
+                res += m * k
+            end
+            k += 1
+        end
+    end
+    res
+end
+
+
+function b(input)
+    for i = length(input):-1:1
+        n, len = input[i]
+        if n != -1
+            k = findfirst(input) do (m, emptylen)
+                m == -1 && len <= emptylen
+            end
+            if !isnothing(k) && k < i
+                _, emptylen = input[k]
+                input = [input[1:k-1]; [(n, len), (-1, emptylen - len)]; input[k+1:i-1]; [(-1, len)]; input[i+1:end]]
+            end
+        end
+    end
+    return checksum2(filter(input) do (m, len)
+        len != 0
+    end)
+end
+
+println("part B")
+println(b(read_input2()))
