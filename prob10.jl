8bbfd06c215db656156a4630226f3398527a6b6a
12ball <pub@janie.page>
2024-12-10 22:04:57 -0600

    1733889897

--- prob10.jl
+++ prob10.jl
@@ -23,7 +23,6 @@ function a(input)
     res = 0
     for (i, row) in enumerate(input)
         for j in findall('0', row)
-            println("start: $i,$j")
             res += length(unique(filter(s -> !isnothing(s), climb(input, (i, j), 0))))
         end
     end
@@ -32,3 +31,28 @@ end
 
 println("part A")
 println(a(read_input()))
+
+function climb2(input, (i, j), k)
+    if !((1 <= i <= length(input)) && (1 <= j <= length(input[1])))
+        0
+    elseif atoi(input[i][j]) != k
+        0
+    elseif k == 9
+        1
+    else
+        climb2(input, (i - 1, j), k + 1) + climb2(input, (i + 1, j), k + 1) + climb2(input, (i, j - 1), k + 1) + climb2(input, (i, j + 1), k + 1)
+    end
+end
+
+function b(input)
+    res = 0
+    for (i, row) in enumerate(input)
+        for j in findall('0', row)
+            res += climb2(input, (i, j), 0)
+        end
+    end
+    res
+end
+
+println("part B")
+println(b(read_input()))
